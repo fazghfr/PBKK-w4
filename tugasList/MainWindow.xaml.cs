c95ac688b288974120f3b1e45fd282bbe52b849c
Ahmad Fauzan Alghifari <96367502+fazghfr@users.noreply.github.com>
2023-09-18 08:54:20 +0700

    Add files via upload

---
+++ tugasList/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace tugasList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddNameButton_Click(object sender, RoutedEventArgs e)
+        {
+            string newName = nameTextBox.Text.Trim();
+
+            if (!string.IsNullOrWhiteSpace(newName))
+            {
+                // Check if the name already exists in the list
+                if (!nameListBox.Items.Contains(newName))
+                {
+                    // Add the name to the ListBox
+                    nameListBox.Items.Add(newName);
+
+                    // Clear the TextBox
+                    nameTextBox.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Name already exists in the list.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid name.");
+            }
+        }
+    }
+}
